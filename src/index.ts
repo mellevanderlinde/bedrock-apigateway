a0a6b8f2bd290d5e7c266417af9f059e46d09072
Melle <melle@git.com>
2024-02-16 14:28:49 +0100

    Remove whitespaces

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ export const handler: Handler = async (event: APIGatewayEvent) => {
   }
 
   const prompt = `\n\nHuman: ${event.queryStringParameters.prompt} \n\nAssistant:`;
-
   const input = {
     modelId: process.env.MODEL_ID,
     contentType: "application/json",
@@ -25,7 +24,6 @@ export const handler: Handler = async (event: APIGatewayEvent) => {
       max_tokens_to_sample: 100,
     }),
   };
-
   const response = await client.send(new InvokeModelCommand(input));
 
   return {
