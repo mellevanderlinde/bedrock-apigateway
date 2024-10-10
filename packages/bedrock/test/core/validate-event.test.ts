b4ca0a0c942d69f424e143a2c3870cca0ca28dfa
Melle <melle@git.com>
2024-10-10 11:40:37 +0200

    Correct test names

--- packages/bedrock/test/core/validate-event.test.ts
+++ packages/bedrock/test/core/validate-event.test.ts
@@ -1,11 +1,11 @@
 import { validateEvent } from "@repo/bedrock/src/core/validate-event";
 
-it("should return prompt if it is present in the event body", () => {
+it("should return prompt if present in the event body", () => {
   const event = { body: "test-prompt" };
   expect(validateEvent(event as never)).toBe("test-prompt");
 });
 
-it("should throw an error if prompt is missing", () => {
+it("should return 400 response if prompt is missing", () => {
   const event = {};
   expect(validateEvent(event as never)).toStrictEqual({
     statusCode: 400,
