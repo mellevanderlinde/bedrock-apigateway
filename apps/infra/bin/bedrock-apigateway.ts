a4d047c891542e944c076cf7ccdf12b4cc626c3d
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-05-27 15:14:40 +0200

    Simplify, stricter tsconfig.json's

--- apps/infra/bin/bedrock-apigateway.ts
+++ apps/infra/bin/bedrock-apigateway.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-import "source-map-support/register";
 import { App } from "aws-cdk-lib";
 import { BedrockApigatewayStack } from "../lib/bedrock-apigateway-stack";
 
--- apps/infra/test/bedrock-apigateway.test.ts
+++ apps/infra/test/bedrock-apigateway.test.ts
@@ -1,10 +1,11 @@
-import { App, assertions } from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import { expect, it } from "vitest";
 import { BedrockApigatewayStack } from "../lib/bedrock-apigateway-stack";
+import { Template } from "aws-cdk-lib/assertions";
 
 it("should match with snapshot", () => {
   const app = new App();
   const stack = new BedrockApigatewayStack(app, "Stack");
-  const template = assertions.Template.fromStack(stack);
-  expect(template.toJSON()).toMatchSnapshot();
+  const template = Template.fromStack(stack);
+  expect(template).toMatchSnapshot();
 });
